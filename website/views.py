83ea21fbff62604eda57b978970c73faaa7b21e8
Azeez Lawal <azeelaw@gmail.com>
2020-03-27 10:39:20 +0100

    Modified the welcome page and added a function

--- website/views.py
+++ website/views.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -5,3 +6,6 @@ from django.http import HttpResponse
 def welcome(request):
     return HttpResponse("Welcome to the meeting Planner!")
 
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
