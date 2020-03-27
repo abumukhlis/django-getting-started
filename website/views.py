f3a1c22e2ceaf74eca73f9200bd6cc36a47f10f3
Azeez Lawal <azeelaw@gmail.com>
2020-03-27 17:59:55 +0100

    Working with templates

--- website/views.py
+++ website/views.py
@@ -5,7 +5,8 @@ from django.http import HttpResponse
 
 
 def welcome(request):
-    return render(request, "website/welcome.html")
+    return render(request, "website/welcome.html",
+                  {"message": "This data was sent from the view to the template."})
 
 
 def date(request):
