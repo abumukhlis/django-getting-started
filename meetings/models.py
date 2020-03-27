73e003b7f9239789e80ba2b5b0d10a5258ebb929
Azeez Lawal <azeelaw@gmail.com>
2020-03-27 16:00:55 +0100

    Created new model

--- meetings/models.py
+++ meetings/models.py
@@ -2,6 +2,14 @@ from datetime import time
 
 from django.db import models
 
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.name}: room {self.room_number} on floor {self.floor}"
+
 
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
