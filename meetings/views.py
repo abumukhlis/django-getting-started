b84b0685c2184e7ea01cbfcf3993536033d5320d
Azeez Lawal <azeelaw@gmail.com>
2020-03-29 08:00:03 +0100

    listing all rooms

--- meetings/views.py
+++ meetings/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 
-from .models import Meeting
+from .models import Meeting, Room
 
 
 def detail(request, id):
@@ -8,6 +8,7 @@ def detail(request, id):
     return render(request, "meetings/detail.html", {"meeting": meeting})
 
 
-
-
+def rooms_list(request):
+    return render(request, "meetings/rooms_list.html",
+                  {"rooms": Room.objects.all()})
 
